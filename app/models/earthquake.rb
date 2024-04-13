64c313901e256e8d3553cee779282cdf5ed55bc5
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-04-13 01:40:34 -0500

    feat: :sparkles: add models with validations

---
+++ app/models/earthquake.rb
@@ -0,0 +1,11 @@
+class Earthquake < ApplicationRecord
+  self.inheritance_column = nil
+
+  validates :title, :external_url, :place, :mag_type, :latitude, :longitude, presence: true
+  validates :magnitude, numericality: { greater_than_or_equal_to: -1.0, less_than_or_equal_to: 10.0 }
+  validates :latitude, numericality: { greater_than_or_equal_to: -90.0, less_than_or_equal_to: 90.0 }
+  validates :longitude, numericality: { greater_than_or_equal_to: -180.0, less_than_or_equal_to: 180.0 }
+  validates :external_id, uniqueness: true
+
+  enum type: { feature: 0 }
+end
